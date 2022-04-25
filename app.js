e2f843c437850cb9bed87cf0dd73c36bf0a21ab0
richirrim <ricardoortega.dev@gmail.com>
2022-04-25 16:52:36 -0500

    Agrega nuevos métodos get.

--- app.js
+++ app.js
@@ -1,12 +1,50 @@
 const express = require('express')
-
+const res = require('express/lib/response')
 const app = express()
-const port = 3000
+const PORT = 3000
+
+const tasks = [
+  {
+    'id': 1,
+    'description': 'Aprender Express Js',
+    'isCompleted': false
+  },
+  {
+    'id': 2,
+    'description': 'Pasiar al Mayoneso',
+    'isCompleted': true
+  },
+  {
+    'id': 3,
+    'description': 'Ver Naruto por 6ta vez:3',
+    'isCompleted': false
+  },
+]
+
 
 app.get('/', (req, res) => {
-  res.send('Hola, Peter😎')
+  res.send('<h1>ola, Peter😎</h1>')
 })
 
-app.listen(port, () => {
-  console.log(`APP corriendo en el puerto: ${port}`)
+app.get('/api/msj', (request, respose) => {
+  respose.send('Chi')
+})
+
+app.get('/api/explorer', (request, respose) => {
+  const explorer = { name: 'Ricardo', msj: 'Q hay, pai.' }
+  respose.send(explorer)
+})
+
+app.get('/api/tasks', (request, respose) => {
+  respose.json(tasks)
+})
+
+app.get('/api/tasks/:id', (request, response) => {
+  const id = request.params.id
+  const task = tasks.filter(task => task.id === id)
+  response.json(task)
+})
+
+app.listen(PORT, () => {
+  console.log(`APP corriendo en el puerto: ${PORT}`)
 })
